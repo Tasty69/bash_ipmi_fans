3cd5fb5ebedd0c67f20fa807ad8752e41a33903b
tasty69 <sam.burrows98@gmail.com>
2022-05-26 17:39:12 +0800

    Initial commit

---
+++ ipmi_fans.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -h | --host)
+            HOST="$2"
+            shift 2
+            ;;
+        -s | --speed)
+            SPEED="$2"
+            shift 2
+            ;;
+        -h | --help)
+            "This script sets fan speeds on Dell PowerEdge iDrac over IPMI"
+            exit 2
+            ;;
+        -* | --*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1")
+            shift
+            ;;
+    esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}"
+
+get_ip () {
+    case $HOST in
+        r510) 
+            IP_ADDRESS="192.168.165.48"
+            ;;
+        r710)
+            IP_ADDRESS="192.168.165.49"
+            ;;
+        *)
+            echo -n "unknown host"
+            exit 1
+            ;;
+    esac
+
+    return $IP_ADDRESS
+}
+
+main () {
+    get_ip
+    
+    ipmitool -I lanplus -H "${IP_ADDRESS}" -U root -P calvin raw 0x30 0x30 0x01 0x00
+    echo "Dell r510 fan control set to manual"
+
+    ipmitool -I lanplus -H "${IP_ADDRESS}" -U root -P calvin raw 0x30 0x30 0x02 0xff 0x"${SPEED}"
+    echo "Fan speed on host ${HOST} set to ${SPEED}%"
+
+    if [[ -n $1 ]]; then
+        echo "Last line of file specified as non-opt/last argument:"
+        tail -1 "$1"
+    fi
+}
+
+
+main
